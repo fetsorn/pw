b4e8701b8398bb9c26477eccafe356b98a567bd4
alexpgc <alexp@gton.capital>
2022-02-12 23:18:56 +0400

    callibrator mock added compiled

---
+++ contracts/mocks/CalibratorProxyMock.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "../interfaces/ICalibratorProxy.sol";
+
+contract CalibratorProxyMock is ICalibratorProxy {
+
+    uint lps; //dummy var to check the logic in the TDD tests
+    uint upOrDown;
+    address vault;
+
+    constructor() {
+        lps = 0; // dummy constructor
+        upOrDown = 0;
+    }
+
+    function calibratePurelyViaPercentOfLPs_UP(
+        IUniswapV2Pair pool,
+        uint256 _liquidity,
+        uint256 n,
+        uint256 d,
+        address to
+    ) external override {
+        lps = _liquidity;
+        upOrDown = 1; // 1 means Up
+        vault = to;
+    }
+
+    function calibratePurelyViaPercentOfLPs_DOWN(
+        IUniswapV2Pair pool,
+        uint256 _liquidity,
+        uint256 n,
+        uint256 d,
+        address to
+    ) external override {
+        lps = _liquidity;
+        upOrDown = 2; // 2 means Down
+        vault = to;
+    }
+
+    function mockCheckLPs() external view returns (uint) {
+        return lps;
+    }
+
+    function mockCheckType() external view returns (uint) {
+        return upOrDown;
+    }
+
+    function mockVault() external view returns (address) {
+        return vault;
+    }
+
+}
